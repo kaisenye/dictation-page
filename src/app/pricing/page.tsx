3a9eca6cfe0549fa1462130a4c6f194c70310f64
kaisenye <kaisenye16@gmail.com>
2025-08-02 16:00:26 -0700

    chore: update copies in pricing

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -13,7 +13,7 @@ export default function Pricing() {
       <div className="container mx-auto px-4 pt-36 pb-24">
         <div className="max-w-4xl mx-auto">
           <h1 className="text-2xl md:text-3xl font-bold mb-4 text-center">
-            Buy me a <span className="text-neutral-700">few cups of</span> coffee(s)?
+            Buy me a <span className="text-neutral-700">few</span> coffee(s)?
           </h1>
 
           <div className="text-center mb-12">
